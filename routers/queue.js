70b75ef8e43dae40c2370cb0b9a6724d2e09a6bc
LAwade <lucasawade46@gmail.com>
2021-03-30 21:01:23 -0400

    update return res

--- routers/queue.js
+++ routers/queue.js
@@ -9,26 +9,25 @@ router.post('/send', (req, res) => {
             if (!erro && res.statusCode == 200) {
                 let data = JSON.parse(response.body)
                 queue.sendToQueue(req.body.queue, { status: 200, message: 'success', queue: req.body.queue, bot: req.body.bot, data: data.data })
-                return res.status(200).send({ status: 200, message: 'success' })
-            } else {
-                return res.status(400).send({ status: 400, message: 'Request fail' })
             }
         })
+        return res.status(200).send({ status: 200, message: 'success' }) 
     } catch (e) {
         return res.status(400).send({ status: 400, message: 'There was a problem your request!' })
     }
 })
 
-router.get('/received/:queue/:bot', (req, res) => {
+router.get('/received/:queue/:bot', (req, res, next) => {
     try {
         queue.consume(req.params.queue, message => {
             let data = JSON.parse(message.content.toString())
-            if(req.params.bot == data.bot){
-                return res.status(200).send({ status: 200, message: 'success', data: data.data })
-            } else{
-                return res.status(404).send({ status: 200, message: 'error', message: 'Not found' })
+            if (req.params.bot == data.bot) {
+               res.status(200).send({ status: 200, message: 'success', data: data.data })
+            } else {
+               res.status(404).send({ status: 200, message: 'error', message: 'Not found' })
             }
         })
+        
     } catch (e) {
         return res.status(400).send({ status: 400, message: 'There was a problem your request!' })
     }
