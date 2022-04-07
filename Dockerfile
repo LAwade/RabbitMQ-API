7d9352a58a6ee5b8ab5362e24bc91d37a2d267c8
Lucas Awade <lucasawade46@gmail.com>
2022-04-07 13:31:45 -0400

    worker service

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,18 @@
 FROM node:alpine
 
+ARG PARAM
+ARG COMMAND
+
 RUN apk update
-
 RUN apk add git
-
 WORKDIR /usr/app
 
 RUN git clone -b develop https://lawade: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@github.com/LAwade/RabbitMQ-API.git
-
 WORKDIR /usr/app/RabbitMQ-API
-
 RUN mv .env_example .env
-
 RUN npm install
-
 EXPOSE 3004
 
-COPY docker-entrypoint.sh ./
+ENTRYPOINT [ ${COMMAND} ]
 
-RUN chmod 755 docker-entrypoint.sh
-
-CMD ["/bin/sh", "docker-entrypoint.sh"]
+CMD [ ${PARAM} ]
--- scripts/worker.js
+++ scripts/worker.js
@@ -1,44 +1,51 @@
-const rabbitmq = require("../middleware/rabbit");
+#!/usr/bin/env node
+
+const amqp = require("amqplib/callback_api");
 const axios = require("axios");
-require("dotenv").config();
 
-let queue = "tibiabot";
+const queue = process.env.QUEUE;
+const host = process.env.HOST;
+const server = process.env.QUEUE;
 
-const server = "http://" + process.argv[2] + "/api/v2/list/";
-
-const sendRequest = async (queue) => {
-  await rabbitmq
-    .connect()
-    .then(async (channel) => await rabbitmq.createQueue(channel, queue))
-    .then(async (channel) => {
-      await channel.prefetch(1);
-      await channel.consume(queue, async (msg) => {
-        let info = JSON.parse(msg.content.toString());
-        await axios
-          .post(`${server}${info.data.list}`, {
-            world: info.data.world,
-            src: info.data.src,
-            server: info.data.server,
-            guild: info.data.guild,
-            url: info.data.url,
-          })
-          .then((message) => {
-            channel.ack(msg);
-          })
-          .catch((error) => {
-            console.log(error);
-            if (error.response?.status == 404) {
-              console.log("API NOT FOUND OR UNAVAILABLE");
-            }
-          });
-      }),
+amqp.connect("amqp://" + host, function (error, connection) {
+  connection
+    .createChannel(function (error, channel) {
+      channel.assertQueue(queue, {
+        durable: true,
+      });
+      channel.prefetch(1);
+      console.log(
+        " [*] Waiting for messages in %s. To exit press CTRL+C",
+        queue
+      );
+      channel.consume(
+        queue,
+        async (msg) => {
+          let info = JSON.parse(msg.content.toString());
+          await axios
+            .post(`${server}${info.data.list}`, {
+              world: info.data.world,
+              src: info.data.src,
+              server: info.data.server,
+              guild: info.data.guild,
+              url: info.data.url,
+            })
+            .then((message) => {
+              channel.ack(msg);
+            })
+            .catch((error) => {
+              console.log(error);
+              if (error.response?.status == 404) {
+                console.log("API NOT FOUND OR UNAVAILABLE");
+              }
+            });
+        },
         {
-          noAck: true,
-        };
+          noAck: false,
+        }
+      );
     })
-    .catch((err) => {
-      console.log(err);
+    .catch((e) => {
+      console.log("CONNECTION FAIL");
     });
-};
-
-sendRequest(queue);
+});
